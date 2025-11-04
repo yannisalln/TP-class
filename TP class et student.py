a3860d5f2a8242702049aca39912ae4b81d79a68
Yannis AULLEN CHOUBRAC <you@example.com>
2025-11-04 14:48:50 +0100

    v2

--- TP class et student.py
+++ TP class et student.py
@@ -25,13 +25,27 @@ from collections import defaultdict
 
 # 2
 
-class Student:
-    def __init__(self, first_name: str, last_name: str, grade: defaultdict):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.grade = grade
+# class Student:
+#     def __init__(self, first_name: str, last_name: str):
+#         self.first_name = first_name
+#         self.last_name = last_name
+#         self.dic_grade = {}
 
-    def __repr__(self):
-        return(self.first_name + " " + self.last_name)
+#     def __repr__(self):
+#         return(self.first_name + " " + self.last_name)
     
-    def
+#     def add_grade(self, topic: str, grade: float):
+#         self.dic_grade[topic] = grade
+
+# try:
+#     student = Student("Achille", "Talon")
+#     student.add_grade("History", 10.)
+#     student.add_grade("History", 12.)
+# except Exception as e:
+#     print('OOPS - There is an issue in your add_grade method.')
+#     print(f"Error message : {e}")
+# else:
+#     print('Congrats ! Your implementation works !')
+
+# 3 
+
