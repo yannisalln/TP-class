410928f50cb3d8707e73e9562a929f3d5dc18ea3
Yannis AULLEN CHOUBRAC <you@example.com>
2025-11-04 14:51:12 +0100

    v3

--- TP class et student.py
+++ TP class et student.py
@@ -49,3 +49,46 @@ from collections import defaultdict
 
 # 3 
 
+# class Student:
+#     def __init__(self, first_name: str, last_name: str):
+#         self.first_name = first_name
+#         self.last_name = last_name
+#         self.dic_grade = {}
+
+#     def __repr__(self):
+#         return(self.first_name + " " + self.last_name)
+    
+#     def add_grade(self, topic: str, grade: float):
+#         self.dic_grade[topic] = grade
+    
+#     def followed_topics(self):
+#         return self.dic_grade.keys()
+
+# try:
+#     student = Student("Achille", "Talon")
+#     student.add_grade("History", 10.)
+#     topics = student.followed_topics()
+#     if len(topics) != 1 or "History" not in topics:
+#         raise Exception(f"Expecting ['History'] got {topics}")
+# except Exception as e:
+#     print('OOPS - There is an issue in your followed_topics method')
+#     print(f"Error message : {e}")
+# else:
+#     print('Congrats ! Your implementation works !')
+
+# 4
+
+class Student:
+    def __init__(self, first_name: str, last_name: str):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.dic_grade = {}
+
+    def __repr__(self):
+        return(self.first_name + " " + self.last_name)
+    
+    def add_grade(self, topic: str, grade: float):
+        self.dic_grade[topic] = grade
+    
+    def followed_topics(self):
+        return self.dic_grade.keys()
