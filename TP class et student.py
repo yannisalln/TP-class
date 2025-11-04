22e218458c804786232504d14a3b0689bff53360
Yannis AULLEN CHOUBRAC <you@example.com>
2025-11-04 14:37:12 +0100

    v1

--- TP class et student.py
+++ TP class et student.py
@@ -1,3 +1,37 @@
 
+from collections import defaultdict
 
-# debut fichier 
+# 1
+
+# class Student:
+#     def __init__(self, first_name: str, last_name: str):
+#         self.first_name = first_name
+#         self.last_name = last_name
+
+#     def __repr__(self):
+#         return(self.first_name + " " + self.last_name)
+
+
+# try:
+#     student = Student("Achille", "Talon")
+#     if repr(student) != "Achille Talon":
+#         raise Exception("There is an issue in your __repr__ method.")
+# except Exception as e:
+#     print('OOPS - There is an issue in your code.')
+#     print(f"Error message : {e}")
+# else:
+#     print('Congrats ! Your implementation works !')
+
+
+# 2
+
+class Student:
+    def __init__(self, first_name: str, last_name: str, grade: defaultdict):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.grade = grade
+
+    def __repr__(self):
+        return(self.first_name + " " + self.last_name)
+    
+    def
